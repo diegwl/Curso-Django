895ec8e61471641d24a52886ed438aef345422e1
diegwl <diegocastanlopes0210@outlook.com>
2023-08-08 13:32:36 -0300

    Inserindo HTML

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.urls import path
 
 def blog(request):
-    return HttpResponse('blog')
+    return render(request, 'blog/index.html')
 
 def exemplo(request):
-    return HttpResponse('exemplo')
+    return render(request, 'blog/exemplo.html')
